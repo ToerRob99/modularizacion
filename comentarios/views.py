8bc82b4162a6c3028e8c9d5307acfbb5c29524aa
Roberto Parra Mesino <robertoparra99@hotmail.com>
2023-08-09 08:12:57 -0500

    se agregaron controladores en las views y urls create y delete

--- comentarios/views.py
+++ comentarios/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-
-from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Comentario
 
 def test(request):
-    return render(
-        request, "index.html", {}
+    return HttpResponse(
+        "Funciona correctamente"
     )
+
+
+def create(request):
+    # comment = Comentario(name="juan", score=5, comment="este es un comentario")
+    # comment.save()
+    comment = Comentario.objects.get_or_create(name="pedro", score=8)
+    return HttpResponse("En esta ruta prueba controlador para guardar")
+
+
+def delete(request):
+    # comment = Comentario.objects.get(id=1)
+    # comment.delete()
+    Comentario.objects.filter(id=2).delete()
+    return HttpResponse("En esta ruta prueba la eliminacion de un registro")
